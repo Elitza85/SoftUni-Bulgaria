d64e4acbaa63a9f053cdb6957021b1ddb6e2f116
Elitza85 <elitza_85@yahoo.co.uk>
2019-07-17 18:27:24 +0300

    Create Program.cs

---
+++ CSharp Advanced/4.StreamsFilesDirectories/1.Lab/P05.SliceAFile/Program.cs
@@ -0,0 +1,24 @@
+string path = "files";
+            string fileName = "input.txt";
+            string outputFile = "output.txt";
+            string filePath = Path.Combine(path, fileName);
+
+            using (var reader = new StreamReader(filePath))
+            {
+                int count = 0;
+                string line = reader.ReadLine();
+
+                using (var writer = new StreamWriter(Path.Combine(path, outputFile)))
+                {
+                    while (line != null)
+                    {
+                        line = $"{++count}. {line}";
+                        Console.WriteLine(line);
+                        writer.WriteLine(line);
+
+                        line = reader.ReadLine();
+                    }
+
+                }
+
+            }
