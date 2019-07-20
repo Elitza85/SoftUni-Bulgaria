777507014f2f683de8d924ae05b66ae3094efb1c
Elitza85 <elitza_85@yahoo.co.uk>
2019-07-20 15:02:13 +0300

    Add files via upload

---
+++ CSharp Advanced/1.Stealer/Spy.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+
+namespace P01.Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string investigatedClass, params string[] fields)
+        {
+            Type classType = typeof(Hacker);
+            FieldInfo[] classFields= classType.GetFields(BindingFlags.Instance |
+                BindingFlags.Static | BindingFlags.NonPublic | BindingFlags.Public);
+
+            var sb = new StringBuilder();
+
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {investigatedClass}");
+
+            foreach (FieldInfo field in classFields.Where(f=>fields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().Trim();
+        }
+    }
+}
