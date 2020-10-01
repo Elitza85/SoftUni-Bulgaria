323bf24c0c72e3910ec67b297ca8ba8fb5909efe
Elica Yordanova <elitza_85@yahoo.co.uk>
2020-10-01 15:49:47 +0300

    CSharp advanced module lab and exercise- tasks solutions

---
+++ CSharp Advanced/8.Generics/2.Ex/9.CustomLinkedList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DoublyLinkedList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var doubleList = new DoublyLinkedList<string>();
+
+            doubleList.AddLast("string");
+            doubleList.AddLast("1234");
+            doubleList.AddLast("3565");
+            doubleList.AddLast("test");
+
+            foreach (var item in doubleList)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
